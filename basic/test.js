b63462329729b70488f5295199b75d7eed8ad721
KP314235 <pkishan065@gmail.com>
2024-11-07 09:52:37 +0000

    config

---
+++ basic/test.js
@@ -0,0 +1,13 @@
+// console.log("Kishan Patel");
+
+function sum(number1, number2, name){
+    let plus=number1 * number2;
+    return plus;
+}
+console.log(sum(7, 8))
+const name = "Kishan Patel";
+function firstName(){
+    name = "test name";
+    return name;
+}
+console.log(firstName())
