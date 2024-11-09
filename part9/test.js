6bebe6bdc2004a2feb16a2cea5eff429eaf37d9b
KP314235 <pkishan065@gmail.com>
2024-11-09 07:27:27 +0000

    confirm

---
+++ part9/test.js
@@ -0,0 +1,53 @@
+// primitive data type
+
+/*
+1) String
+2) Number
+3) Boolean(true, false)
+4) Null
+5) Undefined
+6) Symbole
+7) BigInt
+
+*/
+
+// const Value1 = Symbol("123");
+// const Value2 = Symbol("123");
+
+// console.log(Value1 == Value2);
+
+// console.log(Value1);
+// console.log(Value2);
+
+// let bigNumber = 38787326437264673475645765325763;
+
+// console.log(typeof bigNumber)
+
+// Reference (No Primitive Data type)
+
+/*
+1) Array
+*/
+// let result = [100, 300, 500, 600]
+
+// console.log(result[3]);
+
+/*
+2) Object
+*/
+// let Userdetail = {
+//     name: "Kishan",
+//     age: 27,
+//     IsLogined: true
+// }
+
+// console.log(Userdetail.name, Userdetail.age, Userdetail.IsLogined);
+/*
+3) Function
+*/
+const MyFunction = function(){
+    console.log("K P Patel")
+}
+console.log(MyFunction);
+
+
