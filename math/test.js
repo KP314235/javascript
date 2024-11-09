e5f0b359f3e078fdebf663c097d42fa6ebd5f9fc
KP314235 <pkishan065@gmail.com>
2024-11-09 17:16:54 +0000

    confirm

--- math/test.js
+++ math/test.js
@@ -7,8 +7,8 @@
 // console.log(Math.floor(5.97));
 // console.log(Math.random()*10 + 1);
 
-const min = 10
-const max = 20
+const min = 1
+const max = 6
 
 
 console.log(Math.floor((Math.random()*(max - min))+ min));
