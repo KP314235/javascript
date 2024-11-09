a193a0570c56946ddfdada912a0be4e431b0dc73
KP314235 <pkishan065@gmail.com>
2024-11-09 06:29:59 +0000

    cofirm

---
+++ part6/test.js
@@ -0,0 +1,16 @@
+console.log(+true);
+console.log(+"");
+
+let gameCounter = 100;
+
+++gameCounter;
+
+console.log(gameCounter);
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
---
+++ part7/test.js
@@ -0,0 +1,17 @@
+console.log("Kishan Patel");
+const name="patel"
+function sum(num1, num2){
+    let sum = num1 +num2;
+    return sum;
+}
+
+console.log(sum(3, 5));
+
+let value = Number(sum(3, 5));
+let a = 8;
+
+console.log(value);
+
+let str = "123";
+let num = String(str);
+console.log(num); // Output: 123
