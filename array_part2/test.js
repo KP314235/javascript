3235d3e28ab046fd29cf2d4cd47d2be82f89099a
KP314235 <pkishan065@gmail.com>
2024-11-10 16:45:08 +0000

    confirm

--- array_part2/test.js
+++ array_part2/test.js
@@ -7,6 +7,26 @@ const name2 = ["abc" ,"pqr" , "xyz"];
 
 const newName = name.concat(name2);
 
-console.log(newName);
+// console.log(newName);
 
-console.log(newName[6]);
+const moreName = [...name, ...name2];
+
+// console.log(moreName);
+
+const newArray = [0, 1, 2, 3, 4, [4, 5, 6], 7 ,[7 , [8, 10]]];
+
+const fArray = newArray.flat(2);
+
+// console.log(fArray);
+
+console.log(Array.isArray("Kishan"));
+console.log(Array.isArray(newArray));
+
+console.log(Array.from("Kishan"));
+console.log(Array.from("987656789"));
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1, score2, score3));
