81401bd231138b52fd16eeed6d9f2b608f1ee905
KP314235 <pkishan065@gmail.com>
2024-11-14 14:56:01 +0000

    Confirm

---
+++ while_loop/test.js
@@ -0,0 +1,17 @@
+// while loop 
+
+let value = 5
+while(value >= 0){
+
+    // console.log(value);
+    value--;
+}
+
+// do while loop
+
+let value1 = 6;
+do {
+    console.log(`the value is ${value1}`);
+    value1++;
+
+} while (value1 <= 5);
